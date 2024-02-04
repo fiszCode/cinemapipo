b342b07b5e477eb077180718b565a612d830042b
fiszCode <107449380+fiszCode@users.noreply.github.com>
2024-02-04 16:12:05 -0300

    Correccion de mierdita de key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,34 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
 import React, { Fragment } from "react";
 
-
 export default function Home() {
+  const primas = ["Iae", "Ornella", "Putina"];
+  const presidentes = ["Albert", "Milei", "Cristina"];
 
-  const primas = ['Iae', 'Ornella', 'Putina']
-  const presidentes = ['Albert', 'Milei', 'Cristina']
-  
-
-  return (<div className={styles.main}>
-     <h1>Hola Jorge</h1>
-     {/* <Persona nombre={primas[0]}/>
+  return (
+    <div className={styles.main}>
+      <h1>Hola Jorge</h1>
+      {/* <Persona nombre={primas[0]}/>
      <Persona nombre={primas[1]}/>
      <Persona nombre={primas[2]}/> */}
 
-     <div style={{ margin: '1rem'}}>
-         {primas.map(e => <Persona nombre={e}/>)}
-     </div>
-    
-     <div style={{ margin: '1rem'}}>
-         {presidentes.map(e => <Persona nombre={e}/>)}
-     </div>
+      <div style={{ margin: "1rem" }}>
+        {primas.map((e, i) => (
+          <Persona key={i} nombre={e} />
+        ))}
+      </div>
 
-    {/* (<Persona nombre={primas.map(prima => <div>{prima}</div>)}/> */}
+      <div style={{ margin: "1rem" }}>
+        {presidentes.map((e, i) => (
+          <Persona key={i} nombre={e} />
+        ))}
+      </div>
 
-  </div>);
+      {/* (<Persona nombre={primas.map(prima => <div>{prima}</div>)}/> */}
+    </div>
+  );
 }
 
-
-
-
-
-function Persona({ nombre }: { nombre: string}) {
-
-  return <h2>{nombre}</h2>
+function Persona({ nombre }: { nombre: string }) {
+  return <h2>{nombre}</h2>;
 }
