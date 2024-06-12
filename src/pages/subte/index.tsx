38bc759947334f196c71b27e12a1231dd5700d03
fiszCode <107449380+fiszCode@users.noreply.github.com>
2024-06-12 19:53:23 -0300

    1953

--- src/pages/subte/index.tsx
+++ src/pages/subte/index.tsx
@@ -1,5 +1,7 @@
+// Importamos el componente SubteViw
 import SubteView from "@/views/subte/SubteView";
 
+// El componente que se carga por defecto en la página
 export default function SubtePage() {
     return (
         <SubteView />
